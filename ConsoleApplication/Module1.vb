896ccf99cd1fa03757a5da1f910a8412031a59ce
Sergey Balog <sbalog@lanteria.net>
2018-03-07 16:48:32 +0200

    Lesson 1

--- ConsoleApplication/Module1.vb
+++ ConsoleApplication/Module1.vb
@@ -1,7 +1,26 @@
-﻿Module Module1
+﻿Imports Microsoft.SharePoint
 
+Module Module1
     Sub Main()
+        Using site As New SPSite("http://2013appsrv2/")
+            Using web As SPWeb = site.OpenWeb()
+                For Each list As SPList In web.Lists
+                    Console.WriteLine(String.Format("{0} ({1})",
+                                                    list.Title,
+                                                    list.ItemCount))
+                    For Each item As SPListItem In list.Items
+                        For Each field As SPField In list.Fields.OfType(Of SPField)().Where(Function(x) x.CanBeDeleted OrElse x.InternalName = "Title")
+                            Console.WriteLine(String.Format("{0}{1} ({2})",
+                                                            vbTab,
+                                                        field.Title,
+                                                        If(item(field.InternalName) Is Nothing, String.Empty, item(field.InternalName).ToString)))
 
+                        Next
+                    Next
+                Next
+            End Using
+        End Using
+        Console.WriteLine("Press any key...")
+        Console.ReadKey()
     End Sub
-
 End Module
